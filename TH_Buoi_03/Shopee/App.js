63d097740d839f87eee817a58d933b41d8c2a62b
Khai-Git <kelvinnon28092001@gmail.com>
2023-09-23 16:04:37 +0700

    update code app.js

--- TH_Buoi_03/Shopee/App.js
+++ TH_Buoi_03/Shopee/App.js
@@ -1,10 +1,23 @@
+import react from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
+      
+      <h1>GROW</h1>
+      <h1>YOUR BUSINESS</h1>
+      <br></br>
+      <br></br>
+      <b>We will help you to grow your business using online server</b>
+      <br></br>
+      <br></br>
+      <div>
+        <input type='submit' value='LOGIN'></input>
+        <input type='submit' value='SIGN UP'></input>
+      </div>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +26,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    background: '#FFFFFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
