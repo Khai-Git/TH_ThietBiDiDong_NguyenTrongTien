0443d32d70bd12ed16d0e547f9c99fb5fba94550
Khai-Git <kelvinnon28092001@gmail.com>
2023-09-24 22:32:08 +0700

    90% finish FirtScreen

--- TH_Buoi_03/Shopee/App.js
+++ TH_Buoi_03/Shopee/App.js
@@ -1,12 +1,15 @@
 import react from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Logo from './assets/Circle.png';
+import './css/index.css';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* <Text>Open up App.js to start working on your app!</Text> */}
-      
+      <img className="img" src={Logo}/>
+      <br></br>
+      <br></br>
       <h1>GROW</h1>
       <h1>YOUR BUSINESS</h1>
       <br></br>
@@ -14,20 +17,22 @@ export default function App() {
       <b>We will help you to grow your business using online server</b>
       <br></br>
       <br></br>
-      <div>
-        <input type='submit' value='LOGIN'></input>
-        <input type='submit' value='SIGN UP'></input>
+      <div id='submitbtn'>
+        <input type='submit' value='LOGIN' id='btn'/>
+        <input type='submit' value='SIGN UP' id='btn'/>
       </div>
       <StatusBar style="auto" />
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    background: '#FFFFFF',
+    backgroundColor: '#00ffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
