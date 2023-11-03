11fd352171db35aefec4c9f428b12fb24c74bf64
Khai-Git <kelvinnon28092001@gmail.com>
2023-11-03 17:20:36 +0700

    A

--- TH_Buoi_08/Task_API/Screens/API_Screen_2.js
+++ TH_Buoi_08/Task_API/Screens/API_Screen_2.js
@@ -1,31 +1,39 @@
 import {
-    StyleSheet,
-    View,
-    Image,
-    TouchableOpacity,
-    TextInput,
-    SafeAreaView,
-    Button,
-    Pressable,
-  } from "react-native";
-  import React, { useState } from "react";
-  import { Stack, Text } from "@react-native-material/core";
-  
-  function App({ navigation }) {
-    return (
-      <View style={styles.container}>
-          <Text>Hello</Text>
+  StyleSheet,
+  View,
+  Image,
+  TouchableOpacity,
+  TextInput,
+  SafeAreaView,
+  Button,
+  Pressable,
+} from "react-native";
+import React, { useState } from "react";
+import { Stack, Text } from "@react-native-material/core";
+
+function App({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <View style={{
+        flexDirection: "row",
+      }}>
+        <Image style={{
+          width: "40px",
+          height: "40px",
+        }} source={require("../assets/Icon-Left-Button.png")} />
+        
       </View>
-    );
-  }
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "white",
-      justifyContent: "center",
-      alignItems: "center",
-    },
-  });
-  
-  export default App;
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default App;
